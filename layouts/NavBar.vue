0a5ac4d5a7f86eb76cfc308134dd3ad87d5edb06
DWARF-Markus <mbh@dwarf.dk>
2020-10-03 17:52:50 +0200

    onAuthStateChanged & signup implemented

--- layouts/NavBar.vue
+++ layouts/NavBar.vue
@@ -1,6 +1,5 @@
 <template>
   <nav>
-    <p>Nav bar</p>
     <div>
       <nuxt-link :to="'/'">Home</nuxt-link>
     </div>
@@ -16,17 +15,39 @@
     <div>
       <nuxt-link :to="'/signup'">Sign Up</nuxt-link>
     </div>
+    <div>
+      <button @click="handleSignOut">Sign out</button>
+    </div>
   </nav>
 </template>
 
 <script>
 export default {
   name: 'NavBar',
+  methods: {
+    handleSignOut() {
+      this.$fireAuth
+        .signOut()
+        .then(function () {
+          console.log('sign out succes')
+        })
+        .catch(() => (error) => {
+          console.log('error', error)
+        })
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 nav {
   width: 100%;
+  border: 2px solid lightcoral;
+  padding: 10px;
+  display: flex;
+
+  div {
+    margin: 10px;
+  }
 }
 </style>
